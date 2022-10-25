90b9ec2380c99ccd4c3a2f9da7b0d92e5529b287
AlexisHenry <alexis.henry150357@gmail.com>
2022-10-25 13:49:32 +0200

    Packages/Utils

---
+++ bin/git.sh
@@ -0,0 +1,4 @@
+function __git_autocompletion() {
+	mkdir -p ~/utils;
+	cp ./utils/
+}
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
+function deploy() {
+	rsync -avh --no-perms ./packages/ ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	deploy;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		deploy;
+	fi;
+fi;
+
+unset deploy;
